a4c66ffd473fc85b25cf1c6f2d4b6ced6ff274d5
Ghislain <Gzeleu@gmx.de>
2022-10-29 12:57:48 +0200

    tests

---
+++ Ghis.Windows.Controls.Test/Charts/LineChartTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghis.Windows.Controls.Test.Charts
+{
+    [TestClass]
+    public  class LineChartTest
+    {
+        [TestMethod]
+        public void VerifyVersionDescription()
+        {
+            
+        }
+    }
+}

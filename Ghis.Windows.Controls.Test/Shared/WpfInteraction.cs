fb7f33d74de4df78b70e4b6c50b06de19b61cfc9
Ghislain <Gzeleu@gmx.de>
2022-10-30 21:05:57 +0100

    opmost is dangerous

--- Ghis.Windows.Controls.Test/Shared/WpfInteraction.cs
+++ Ghis.Windows.Controls.Test/Shared/WpfInteraction.cs
@@ -14,14 +14,14 @@ namespace Ghis.Windows.Controls.Test.Shared
             //pack://application:,,,/MyLib;component/SettingsPage.xaml           
             var allResources = new ResourceDictionary() { Source = new Uri(@"Ghis.Windows.Controls;component\Themes\Generic.xaml", UriKind.Relative) };
             ghisApplication.Resources.MergedDictionaries.Add(allResources);
-            ghisApplication.MainWindow = new Window() { Width=widthWindow, Height=heightWindow, Content=contentWindow, Title= titleWindow, ToolTip=titleWindow, Topmost=true };
+            ghisApplication.MainWindow = new Window() { Width=widthWindow, Height=heightWindow, Content=contentWindow, Title= titleWindow, ToolTip=titleWindow, Topmost=false };
             ghisApplication.MainWindow.ShowDialog();
 
         }
         public static void Show(object contentWindow, int widthWindow = 955, int heightWindow = 555, string titleWindow = "")
         {
             var ghisApplication = new Application();
-            ghisApplication.MainWindow = new Window() { Width=widthWindow, Height=heightWindow, Content=contentWindow, Title= titleWindow, ToolTip=titleWindow, Topmost=true };
+            ghisApplication.MainWindow = new Window() { Width=widthWindow, Height=heightWindow, Content=contentWindow, Title= titleWindow, ToolTip=titleWindow, Topmost=false };
             ghisApplication.MainWindow.Show();
 
         }

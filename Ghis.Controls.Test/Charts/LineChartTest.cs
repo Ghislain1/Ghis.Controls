1668dee963751ed2c3b7bc6b7d0056fbab0fdab7
Ghislain1 <gzeleu@outlook.de>
2023-09-02 12:16:19 +0200

    Add Category to filter test

--- Ghis.Controls.Test/Charts/LineChartTest.cs
+++ Ghis.Controls.Test/Charts/LineChartTest.cs
@@ -1,25 +1,33 @@
-﻿namespace Ghis.Controls.Test.Charts
+﻿namespace Ghis.Controls.Test.Charts;
+
+using Ghis.Controls.Charts;
+using Ghis.Controls.Test.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+[TestClass]
+public class LineChartTest
 {
-    using Ghis.Controls.Test.Shared;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    // TODO:
+    // 1. Write test in output console for tests
+    // use edit config
 
-    [TestClass]
-    public class LineChartTest
+    [WpfUserControlTestMethod]
+    public void Show_LineChart_InAction_Test()
     {
-        // TODO:
-        // 1. Write test in output console for tests
-        // use edit config
-
-        [WpfUserControlTestMethod]
-        public void Show_LineChart_InAction_Test()
-        {
-            // The following line will not fail unless executed on an STA thread.
-            var content = new LineChartView();
-            content.DataContext = new LineChartViewModel();
-            WpfInteraction.ShowDialog(content, titleWindow: "Line Chart in action");
-
-        }
-
+        // The following line will not fail unless executed on an STA thread.
+        var content = new LineChartView();
+        content.DataContext = new LineChartViewModel();
+        WpfInteraction.ShowDialog(content, titleWindow: "Line Chart in action");
 
     }
+    [TestMethod]
+    public void TestLine_Init_Property_True()
+    {
+        // In General is Dat for Fluentassertions
+        var line = new LineChart();
+        line.ValueAxisItemTemplate.Should().NotBeNull();
+    }
+
+
 }
